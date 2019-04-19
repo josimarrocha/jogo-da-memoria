89e29e409ee847b55b69e42ab04457cd6f3401e6
Josimar <rocha_josimar@yahoo.com.br>
2019-04-19 16:57:17 +0000

    adicionando estilo, funcionalidades e colocando as classes no padrao BEM

---
+++ js/main.js
@@ -0,0 +1,117 @@
+const xrh = new XMLHttpRequest()
+xrh.addEventListener('readystatechange')
+(function () {
+	'use strict'
+	const imgCard = [
+		'svg1.svg',
+		'svg2.svg',
+		'svg3.svg',
+		'svg4.svg',
+		'svg5.svg',
+		'svg6.svg',
+		'svg7.svg',
+		'svg8.svg',
+		'svg9.svg'
+	]
+	const $game = document.querySelector('.game-memory')
+	const $btnCategoria = document.querySelector('.btn-categoria')
+	const $categoriaMenu = document.querySelector('.categoria-menu')
+	const $radioCategoria = document.querySelectorAll('.radio-categoria')
+	const $congratulations = document.querySelector('.congratulations')
+	const countPares = []
+	$btnCategoria.addEventListener('click', openMenuCategoria)
+
+	function openMenuCategoria() {
+		$btnCategoria.classList.toggle('openMenu')
+		$categoriaMenu.classList.toggle('-active')
+	}
+
+	function app(categoria){
+		imgCard.forEach((img) => {
+			const cards = `
+			<div class="card" data-js='${img}'>
+				<div class="card__back">
+					<img src='img/${categoria}/${img}' class='card__img' />
+				</div>
+				<div class="card__front">
+					<img src='img/${categoria}/capa.svg' class='card__img' />
+				</div>
+			</div>`
+			$game.innerHTML += cards + cards
+		})
+
+		$radioCategoria.forEach((radio) => {
+			radio.addEventListener('click', () => {
+				$game.innerHTML = ''
+				app(radio.getAttribute('data-radio'))
+			})
+		})
+
+		let $card = document.querySelectorAll('.card')
+
+		function spreadCards() {
+			$card.forEach((card) => {
+				setTimeout(() => card.classList.toggle(`-animacao`), 400)
+				card.addEventListener('click', handleOpenCard)
+			})
+		}
+
+		(function () {
+			$card.forEach((card) => {
+				let random = Math.floor(Math.random() * 12 + 1)
+				card.style.order = random
+				random = ''
+			})
+		})()
+
+		let firstCart;
+		let secondCart;
+
+		function handleOpenCard() {
+			$btnCategoria.classList.remove('openMenu')
+			$categoriaMenu.classList.remove('-active')
+			if (!secondCart) {
+				this.classList.add('-flip')
+				if (firstCart) {
+					secondCart = this
+					firstCart.addEventListener('click', handleOpenCard)
+					isEqualsCards()
+					return false
+				}
+				firstCart = this
+				firstCart.removeEventListener('click', handleOpenCard)
+			}
+		}
+
+		function isEqualsCards() {
+			firstCart.getAttribute('data-js') !== 
+			secondCart.getAttribute('data-js') ?
+				disableFlip() : removeEventClick()
+		}
+
+		function disableFlip() {
+			setTimeout(() => {
+				secondCart.classList.remove('-flip')
+				firstCart.classList.remove('-flip')
+				firstCart = ''
+				secondCart = ''
+			}, 2000)
+		}
+
+		function removeEventClick() {
+			countPares.push(1)
+			if(imgCard.length === countPares.length){
+				$congratulations.style.display = 'block'
+			}
+			firstCart.removeEventListener('click', handleOpenCard);
+			secondCart.removeEventListener('click', handleOpenCard);
+			firstCart = '';
+			secondCart = '';
+		}	
+		spreadCards()
+	}
+	app('TI')
+})(window)
+
+
+
